2b55a34fcdf880fab6c627dc7820c79148deb901
いっとく <73953416+ittoku703@users.noreply.github.com>
2022-12-14 22:12:47 +0900

    Updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,33 @@
 puts 'db seed:'
-Post.create!(title: 'First post', content: 'Hello World!!!');
-Post.create!(title: 'x' * 999, content: 'x' * 99999);
+
+post = Post.create!(title: 'First post', content: 'Hello World!!!')
+Comment.create!(content: '1 Comment', post: post)
+
+too_long_post = Post.create!(title: 'x' * 999, content: 'x' * 99_999)
+Comment.create!(content: 'x' * 999, post: too_long_post)
+
+posts_hash = []
 
 50.times do |i|
-  title = 'Post: ' + i.to_s;
-  content = 'loop ' + i.to_s + ' times';
-  p Post.create!(title: title, content: content)
+  hash = {}
+  hash['title'] = "Post: #{i}"
+  hash['content'] = "loop #{i} times"
+  posts_hash << hash
 end
+
+posts = Post.create!(posts_hash)
+puts 'posts created:'
+puts posts
+
+comments_hash = [] 
+
+50.times do |i|
+  hash = {}
+  hash['content'] = "#{i} Comment"
+  hash['post'] = posts[rand(0..9)]
+  comments_hash << hash
+end
+
+comments = Comment.create!(comments_hash)
+puts 'comments created:'
+puts comments
