8df398daadb6d57ab4ee43579a7679948c11e5cb
ittokun <73953416+ittokun@users.noreply.github.com>
2023-03-02 07:18:51 +0900

    #12 Add posts Model

---
+++ src/db/models.rs
@@ -0,0 +1,20 @@
+use diesel::prelude::*;
+use serde::{Deserialize, Serialize};
+
+use crate::db::schema::posts;
+
+#[derive(Debug, Deserialize, Serialize, Queryable)]
+pub struct Post {
+    pub id: i32,
+    pub title: String,
+    pub body: String,
+    pub published: bool,
+}
+
+#[derive(Debug, Insertable, Serialize, Deserialize)]
+#[diesel(table_name = posts)]
+pub struct NewPost {
+    pub title: String,
+    pub body: String,
+    pub published: Option<bool>,
+}
