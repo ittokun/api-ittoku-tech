46ba25840d795a046753e33a3f4d5faccf2b56e9
ittokun <73953416+ittokun@users.noreply.github.com>
2023-03-02 20:24:01 +0900

    #12 Implemented GET /posts

--- src/api/posts.rs
+++ src/api/posts.rs
@@ -1,8 +1,18 @@
+use crate::db::schema::posts::dsl::*;
+use crate::db::establish_connection;
+use crate::db::models::Post;
+
 use actix_web::{get, post, patch, delete, web, HttpResponse};
+use diesel::prelude::*;
 
 #[get("/posts")]
 async fn list() -> HttpResponse {
-    HttpResponse::Ok().body("posts list")
+    let connection = &mut establish_connection();
+    let results: Vec<Post> = posts
+        .load::<Post>(connection)
+        .expect("Error loading posts");
+
+    HttpResponse::Ok().json(results)
 }
 
 #[post("/posts")]
@@ -10,23 +20,23 @@ async fn create() -> HttpResponse {
     HttpResponse::Ok().body("posts create")
 }
 
-#[get("/posts/{id}")]
+#[get("/posts/{post_id}")]
 async fn detail(path: web::Path<u32>) -> HttpResponse {
-    let id = path.into_inner();
+    let post_id = path.into_inner();
 
-    HttpResponse::Ok().body(format!("posts detail {}", id))
+    HttpResponse::Ok().body(format!("posts detail {}", post_id))
 }
 
-#[patch("/posts/{id}")]
+#[patch("/posts/{post_id}")]
 async fn update(path: web::Path<u32>) -> HttpResponse {
-    let id = path.into_inner();
+    let post_id = path.into_inner();
 
-    HttpResponse::Ok().body(format!("posts update {}", id))
+    HttpResponse::Ok().body(format!("posts update {}", post_id))
 }
 
-#[delete("/posts/{id}")]
+#[delete("/posts/{post_id}")]
 async fn delete(path: web::Path<u32>) -> HttpResponse {
-    let id = path.into_inner();
+    let post_id = path.into_inner();
 
-    HttpResponse::Ok().body(format!("posts delete {}", id))
+    HttpResponse::Ok().body(format!("posts delete {}", post_id))
 }
