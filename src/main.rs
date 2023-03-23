f8dd481164114e41025e665c291eb41b3c22668a
ittokun <73953416+ittokun@users.noreply.github.com>
2023-03-23 21:59:05 +0900

    Add Environment variables and Logging (#19)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+#[macro_use]
+extern crate log;
+
+use actix_web::middleware::Logger;
 use actix_web::{get, App, HttpResponse, HttpServer};
+use std::env;
 
 #[get("/")]
 async fn index() -> HttpResponse {
@@ -7,8 +12,14 @@ async fn index() -> HttpResponse {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
-        .bind("0.0.0.0:8080")?
+    env_logger::init();
+
+    let host = env::var("HOST").expect("Host not set");
+    let port = env::var("PORT").expect("Port not set");
+
+    info!("Starting Server: http://{host}:{port}");
+    HttpServer::new(|| App::new().wrap(Logger::default()).service(index))
+        .bind(format!("{host}:{port}"))?
         .run()
         .await
 }
