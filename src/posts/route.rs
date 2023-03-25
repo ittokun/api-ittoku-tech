08c6ea984fac8197bfd36a9e992f0fd5d9230bb4
ittokun <73953416+ittokun@users.noreply.github.com>
2023-03-26 05:29:30 +0900

    fix: :art: Pretty JSON

--- src/posts/route.rs
+++ src/posts/route.rs
@@ -1,40 +1,46 @@
 use crate::api_error::ApiError;
 use crate::posts::*;
 use actix_web::{delete, get, patch, post, web, HttpRequest, HttpResponse, Result};
+use serde_json::to_string_pretty;
 use uuid::Uuid;
 
 #[get("/posts")]
 async fn find_all() -> Result<HttpResponse, ApiError> {
     let posts = Post::find_all()?;
     let posts = PostFindAll::new(posts.len(), posts);
-    Ok(HttpResponse::Ok().json(posts))
+    let posts = to_string_pretty(&posts).unwrap();
+    Ok(HttpResponse::Ok().body(posts))
 }
 
 #[get("/posts/{id}")]
 async fn find(req: HttpRequest) -> Result<HttpResponse, ApiError> {
     let id: Uuid = req.match_info().query("id").parse().unwrap_or_default();
     let post = Post::find(id)?;
-    Ok(HttpResponse::Ok().json(post))
+    let post = to_string_pretty(&post).unwrap();
+    Ok(HttpResponse::Ok().body(post))
 }
 
 #[post("/posts")]
 async fn create(post: web::Json<PostParams>) -> Result<HttpResponse, ApiError> {
     let post = Post::create(post.into_inner())?;
-    Ok(HttpResponse::Created().json(post))
+    let post = to_string_pretty(&post).unwrap();
+    Ok(HttpResponse::Created().body(post))
 }
 
 #[patch("/posts/{id}")]
 async fn update(req: HttpRequest, post: web::Json<PostParams>) -> Result<HttpResponse, ApiError> {
     let id: Uuid = req.match_info().query("id").parse().unwrap_or_default();
     let post = Post::update(id, post.into_inner())?;
-    Ok(HttpResponse::Ok().json(post))
+    let post = to_string_pretty(&post).unwrap();
+    Ok(HttpResponse::Ok().body(post))
 }
 
 #[delete("/posts/{id}")]
 async fn delete(req: HttpRequest) -> Result<HttpResponse, ApiError> {
     let id: Uuid = req.match_info().query("id").parse().unwrap_or_default();
     let post = Post::delete(id)?;
-    Ok(HttpResponse::Ok().json(post))
+    let post = to_string_pretty(&post).unwrap();
+    Ok(HttpResponse::Ok().body(post))
 }
 
 pub fn init_routes(cfg: &mut web::ServiceConfig) {
