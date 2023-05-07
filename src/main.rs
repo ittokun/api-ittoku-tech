53556193227740db82f07efe49e6b9ebf82e51cf
ittokun <73953416+ittokun@users.noreply.github.com>
2023-05-08 04:13:33 +0900

    feat: :sparkles: Defined api
    Issue: #33

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,38 @@
-#[macro_use]
-extern crate log;
+// #[macro_use]
+// extern crate log;
 
-use actix_web::middleware::Logger;
-use actix_web::{App, HttpServer};
-use std::env;
+// use actix_web::middleware::Logger;
+// use actix_web::{App, HttpServer};
+// use std::env;
 
-mod api_error;
-mod comments;
-mod config;
-mod db;
-mod posts;
-mod schema;
-#[cfg(test)]
-mod test;
+// mod api_error;
+// mod comments;
+// mod config;
+// mod db;
+// mod posts;
+// mod schema;
+// #[cfg(test)]
+// mod test;
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    env_logger::init();
-    db::init();
+fn main() {
+    api::main();
+    // env_logger::init();
+    // db::init();
 
-    let host = env::var("HOST").expect("Host not set");
-    let port = env::var("PORT").expect("Port not set");
-    let app = || {
-        App::new()
-            .wrap(Logger::default())
-            .service(ittoku_api::index)
-            .configure(config::init)
-            .configure(posts::init_routes)
-            .configure(comments::init_routes)
-    };
+    // let host = env::var("HOST").expect("Host not set");
+    // let port = env::var("PORT").expect("Port not set");
+    // let app = || {
+    //     App::new()
+    //         .wrap(Logger::default())
+    //         .service(ittoku_api::index)
+    //         .configure(config::init)
+    //         .configure(posts::init_routes)
+    //         .configure(comments::init_routes)
+    // };
 
-    info!("Starting Server: http://{host}:{port}");
-    HttpServer::new(app)
-        .bind(format!("{host}:{port}"))?
-        .run()
-        .await
+    // info!("Starting Server: http://{host}:{port}");
+    // HttpServer::new(app)
+    //     .bind(format!("{host}:{port}"))?
+    //     .run()
+    //     .await
 }
