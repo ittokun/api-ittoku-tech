a3ee924e42c449e335b51fb68137b19fe7ac57cd
ittokun <73953416+ittokun@users.noreply.github.com>
2023-05-10 04:37:17 +0900

    feat: :heavy_plus_sign: Add DatabaseConnection to api
    Issue #38

--- crates/api/main.rs
+++ crates/api/main.rs
@@ -1,24 +1,40 @@
 #[macro_use]
 extern crate log;
 
+use db::{get_db_connection, migrate};
+use db::sea_orm::DatabaseConnection;
+
 use actix_web::middleware::Logger;
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpServer};
 
 use std::env;
 
 mod routes;
 
+#[derive(Debug, Clone)]
+pub struct AppState {
+    db: DatabaseConnection,
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init();
-
     // get env variables
     let host = env::var("HOST").expect("HOST must be set");
     let port = env::var("PORT").expect("PORT must be set");
     let base_url = env::var("BASE_URL").expect("BASE_URL must be set");
     let server_url = format!("{}:{}", host, port);
 
-    let app = || App::new().wrap(Logger::default()).configure(routes::init);
+    env_logger::init();
+    migrate().await.expect("Failed to migrate");
+
+    let db = get_db_connection().await.unwrap();
+    let state = AppState { db };
+    let app = move || {
+        App::new()
+            .app_data(web::Data::new(state.clone()))
+            .wrap(Logger::default())
+            .configure(routes::init)
+    };
 
     info!("Starting server at: {}", base_url);
     HttpServer::new(app).bind(server_url)?.run().await
--- crates/db/core/lib.rs
+++ crates/db/core/lib.rs
@@ -1,43 +1,24 @@
-mod mutation;
-mod query;
-
-pub use mutation::Mutation;
-pub use query::Query;
-
+use entity::sea_orm::{Database, DatabaseConnection, DbErr};
+use migration::sea_orm_migration::MigratorTrait;
 use migration::Migrator;
-
-use entity::{
-    post,
-    sea_orm::{Database, DbErr},
-};
-use migration::sea_orm_migration::{MigratorTrait, SchemaManager};
 
 use std::env;
 
-pub async fn run() -> Result<(), DbErr> {
+pub use entity::sea_orm;
+pub use mutation::Mutation;
+pub use query::Query;
+
+mod mutation;
+mod query;
+
+pub async fn get_db_connection() -> Result<DatabaseConnection, DbErr> {
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL not set");
-
     let db = Database::connect(db_url).await?;
+    Ok(db)
+}
+
+pub async fn migrate() -> Result<(), DbErr> {
+    let db = get_db_connection().await?;
     Migrator::up(&db, None).await?;
-
-    let schema_manager = SchemaManager::new(&db);
-    assert!(schema_manager.has_table("post").await?);
-
-    let post = Mutation::create_post(
-        &db,
-        post::NewModel {
-            title: "Hello".to_owned(),
-            text: "World".to_owned(),
-        },
-    )
-    .await
-    .expect("Failed create post");
-    let post = Query::find_post_by_id(&db, post.id.unwrap())
-        .await
-        .expect("Failed update post");
-    assert_eq!(post.unwrap().title, "Hello");
-
-    Mutation::delete_all_posts(&db).await.unwrap();
-
     Ok(())
 }
